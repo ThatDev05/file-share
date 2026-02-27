e3357c5f6cf8624c3f04fb702aed483220169bcd
thatdev05 <deepb9945@gmail.com>
2026-02-27 12:17:39 +0530

    chore: clear unstaged changes

--- file-share-frontend-react/src/App.jsx
+++ file-share-frontend-react/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <div className="app-container">
         <a href="/">
-          <img src="https://send.creativeshi.com/logo.png" alt="Inshare logo" className="logo" />
+          <img src="/logo.png" alt="Inshare logo" className="logo" />
         </a>
         <Routes>
           <Route path="/" element={<Home />} />
