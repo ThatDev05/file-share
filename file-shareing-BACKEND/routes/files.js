4533ab4fede3a2e1b63670641fe95a0ddd27ad95
thatdev05 <deepb9945@gmail.com>
2026-02-28 10:50:21 +0530

    Move /info/:uuid to /api/files to match frontend API request

--- file-shareing-BACKEND/routes/files.js
+++ file-shareing-BACKEND/routes/files.js
@@ -179,4 +179,21 @@ router.get('/pin/:pin', async (req, res) => {
     }
 });
 
+router.get('/info/:uuid', async (req, res) => {
+    try {
+        const file = await File.findOne({ uuid: req.params.uuid });
+        if (!file) {
+            return res.status(404).json({ error: 'Link has expired' });
+        }
+        return res.json({
+            uuid: file.uuid,
+            fileName: file.filename,
+            fileSize: file.size,
+            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+        });
+    } catch (err) {
+            return res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+
 module.exports = router;
